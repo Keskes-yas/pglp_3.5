5900b0ede88ddb6497ead119ad0e8fb95c28450a
Keskes-yas <61762693+Keskes-yas@users.noreply.github.com>
2020-05-24 23:48:59 +0200

    LAst commit

--- DIP/src/main/java/Prog/gl/DIP/App.java
+++ DIP/src/main/java/Prog/gl/DIP/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        metier m =  new metier();
-        m.metier();
+        System.out.println( "Hello World!" );
     }
 }
